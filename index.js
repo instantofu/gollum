a2b6675719bf9dc4e0784281106886ab2ecfac55
Hoa <instantofu@gmail.com>
2021-05-31 12:26:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var names = ["Tam", "Muneeb", "Hoa", "Maida", "Dennis", "Srijana", "Hadi", "Dirk"];
 
 
-function whosPaying() {
+function whosNextGollum() {
 
     var numberOfPeople = names.length;
     var randomPersonPosition = Math.floor(Math.random() * numberOfPeople); //0-0.99999 0.4.99999
@@ -11,6 +11,6 @@ function whosPaying() {
   
 }
 
-nextGollum = whosPaying(names);
+nextGollum = whosNextGollum(names);
 
 document.querySelector("h1").innerHTML = "next Gollum is " + nextGollum + " !";
